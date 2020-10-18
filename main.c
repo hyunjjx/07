a62181119bdde7fe07d8efc5d20ffcbfe525efda
jeongxhj <55428319+jeongxhj@users.noreply.github.com>
2020-10-18 19:27:04 +0900

    01

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+
+	int flag = 1;
+	
+	while(flag !=0)
+	{ //y의 범위가 while함수 안에 있는거라서 
+		int y;
+	 
+		y=3;
+		flag=0; 
+	}
+
+	y=4;
+
+	return 0;
+}
